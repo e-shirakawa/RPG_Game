5c98291280832b191308dbd31f61cd6ed2ef0e75
Eiki Shirakawa <e.shirakawa.s@gmail.com>
2026-08-20 09:30:20 +0900

    ファイル名変更

---
+++ sou_itemset.c
@@ -0,0 +1,195 @@
+#include<stdio.h>
+#include<string.h>
+#include"RPG.h"
+
+
+
+
+void itemset(ITEM items[])
+{
+
+//===================
+//攻撃系アイテム
+//===================
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+
+
+//===========================
+//HP回復系アイテム
+//===========================
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+
+
+//===========================
+//MP回復系アイテム
+//===========================
+
+    strcpy(items[0].name, "エーテル");
+    items[0].powerpoint = 50;
+    items[0].type = 3;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+    strcpy(items[0].name, "薬草");
+    items[0].powerpoint = 120;
+    items[0].type = 2;
+
+}
